5341e1a418cfba4d381defb55f0c2b5353d1769f
nowke <navaneeshkumarb@gmail.com>
2015-12-28 19:41:12 +0530

    Fetch from production server

--- app/src/main/java/in/nowke/courseview/classes/Constants.java
+++ app/src/main/java/in/nowke/courseview/classes/Constants.java
@@ -4,7 +4,9 @@ package in.nowke.courseview.classes;
  * Created by nav on 23/12/15.
  */
 public class Constants {
-    public static final String BASE_URL = "http://192.168.1.104:5200";
+//    public static final String BASE_URL = "http://192.168.1.104:5200";
+    public static final String BASE_URL = "http://courseview-nowke.herokuapp.com";
+
     public static final String DOCUMENTS_URL = BASE_URL + "/api/v1/documents/";
 
     public static String getSubjectsForDocument(Integer documentId) {
